0a65384e1b53ef7984da92f8a114e7602ae7621f
Sylvain Usai <usai@designtoproduction.com>
2024-02-03 15:07:59 +0100

    define default config json and mapping class

---
+++ src/mapping/mapper.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+import json
+
+
+
+
+class Mapper():
+
+    def __init__(self, configFileName):
+        self.ConfigFileName = configFileName
+        self.MappingDictionary = self.LoadJson()
+
+
+    def LoadMappingDictionary(self):
+        # Find the directory of the current file
+        dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
+
+        # Compose json config path
+        json_file = "%s\mapping_config\%s"%(dir_path.__str__(), self.ConfigFileName)
+
+        # Read json content 
+        with open(json_file) as json_data:
+             return json.load(json_data)
+    
+    def ConvertAttributeKey(self, sourceKey):
+        """try to convert the input key using the loaded mapping dictionnary.
+        Returns the targetKey if source is found in the config. Returns the original key otherwise.
+        """
+        return self.MappingDictionary.get(sourceKey, sourceKey)
+
+print ("starting")
+mapper = Mapper("mapping_X2Y.json")
