cb25333e884225bed0648f86ce47e3f607bedfca
Knuttatutta <knut.tjensvoll@hotmail.com>
2024-02-03 15:42:25 +0100

    flask server

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from src.converter.converter import IfcConverter
+import requests
 
 target_url = 'https://speckle.xyz/streams/cb3735a1c9/branches/test'
 token = '902d92398804e5f0afe1f1c92ea89e8a48797d4ffa'
-file = 'response.ifc'
+file = 'response2.ifc'
+
+url = 'https://cdn.derivative.autodesk.com/dXJuOmFkc2sud2lwcHJvZDpmcy5maWxlOnZmLnJoVE5NdUthUTFhblVobUs3M0JLb2c_dmVyc2lvbj0z/output/Resource/IFC/HackathonExampleProject.ifc?Policy=eyJTdGF0ZW1lbnQiOiBbeyJSZXNvdXJjZSI6Imh0dHBzOi8vY2RuLmRlcml2YXRpdmUuYXV0b2Rlc2suY29tL2RYSnVPbUZrYzJzdWQybHdjSEp2WkRwbWN5NW1hV3hsT25abUxuSm9WRTVOZFV0aFVURmhibFZvYlVzM00wSkxiMmNfZG1WeWMybHZiajB6L291dHB1dC9SZXNvdXJjZS9JRkMvSGFja2F0aG9uRXhhbXBsZVByb2plY3QuaWZjIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNzA2OTkwNTYyfX19XX0_&Key-Pair-Id=APKAIJ4JBLIOQMJEURDQ&Signature=BRju0t6-QtNK0Em41fpyHS3Q1CESzjUsumwOFMAT6S2mzWEpUKzVZ7hCr3XeuWIdeDVtfx5isIBbieU0uU2Gj8vCPtuIrIoQKC0mgSXmn3oxD7cpCAILVLG5SfXj5tpBsbdlaOh00HxZwrPc4VvPnQAH2xKz68bGhjGOYSjrYWCCYtH9fdwnxtPclPcEWmN2YaeLMzQjKG7cGlbR5TFyfe2fJ7lb6qYFFMhiMDzLe0wTExp4cuHQ7XPDPaFLwxXOt1UT4GcT8qz79eiivactWQsPQTZSovtRBKoyqks9XuPq8muM~wi8FaGEef-uDeCAD5Oa4P3I~r-ffFx6iP-GbA__'
+
+response = requests.get(url)
+with open(file, 'wb') as f:
+    f.write(response.content)
 
 converter = IfcConverter()
 converter.open_ifc(file)
--- src/app.py
+++ src/app.py
@@ -1,9 +1,36 @@
+from pathlib import Path
+import os
+
 from flask import Flask
+from flask import request
+import requests
+from src.converter.converter import IfcConverter
 
 app = Flask(__name__)
+
 
 @app.get('/ifc_to_speckle')
 def ifc_to_speckle():
 
+    target_url = 'https://speckle.xyz/streams/cb3735a1c9/branches/test'
+    token = '902d92398804e5f0afe1f1c92ea89e8a48797d4ffa'
+    file = Path('temp/response.ifc')
+    file.parent.mkdir(parents=True)
+
+    url = request.args.get('url')
+
+    response = requests.get(url)
+    with open(file, 'wb') as f:
+        f.write(response.content)
+
+    converter = IfcConverter()
+    converter.open_ifc(file)
+    converter.to_speckle(token, target_url)
+
+    os.remove(file)
+
+    return 'Hello world!'
 
 
+if __name__  == '__main__':
+    app.run(host='0.0.0.0', port=5000)
